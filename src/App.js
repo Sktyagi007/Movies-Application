93b7900ac92b2dfda2608a89390d02a20a53597e
Sktyagi007 <sarthak765888@gmail.com>
2022-08-14 23:55:35 +0530

    movies-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Banner from "./Components/Banner";
+import MoviesList from "./Components/MoviesList";
+import Fav from "./Components/Fav";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<><Banner/><MoviesList/></>}></Route>
+        <Route path="/favourites" element={<Fav/>} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
